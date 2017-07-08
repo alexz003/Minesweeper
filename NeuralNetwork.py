cab2bbea0441fc8b5386dd4e76a99ee28fd61c2d
Alex <johnalexmarich@gmail.com>
2017-07-08 13:47:09 -0400

    Fixed feed-forward algorithm... I think.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,42 +1,43 @@
+import math
 import numpy as np
 
+import Minesweeper
+
 class NeuralNetwork():
-			
+	
+	def __init__(self, game):
+		self.weights = np.zeros((0, 0), dtype=np.float)
+		self.game = game
+		self.num_layers = 1
+		self.num_trials = 10000
+		
 	# Sigmoid with vectors
 	def sigmoid(x):
-		return 1/(1 + np.exp(x))
+		return 1/(1 + math.exp(x))
 
 	# Sigmoid derivative with vectors
 	def div_sigmoid(x):
 		return sigmoid(x)*(1-sigmoid(x))
 
 	# Create the neural net model
-	def create_model(num_layers = 1, nodes_per_layer, num_trials=10000):
-
+	def create_model(self, num_inputs, nodes_per_layer, num_layers=1, num_trials=10000):
 		# NxM weights matrix with random initial values
-		weights = np.random.random_uniform((num_layers, nodes_per_layer))
-		
-		# Output matrix
-		output = np.zeros(num_inputs)
-
-		# Run each trial
-		for i in range(num_trials):
-			# Set weights based on trial
-			weights = run_trial(None, np.array(0), weights, output)
-
-	# Run a single trial of Neural Network
-	def run_trial(game, inputs, weights):
-
-		# Make new game if not created or not running
-		if game == None or not game.getStatus() == 0:
-			game = Minesweeper.Minesweeper()
-			game.start_game()	
+		self.weights = np.random.random_sample((num_layers, num_inputs*nodes_per_layer))
 	
-		# Train this test case	
-		weights = train(game, weights)	 
+	# Runs all trials and trains the network	
+	def run_all_trials(self, game, num_trials=10000):
+
+		for i in range(num_trials):
+			# Make new game if not created or not running
+			if game.getBoard() == None or not game.gameStatus() == 0:
+				game = Minesweeper.Minesweeper()
+				game.start_game()	
+	
+			# Train this test case	
+			self.weights = self.train(game, self.weights)	 
 		
 	# Feedforward on weights with board and return outputs
-	def test(game, weights):
+	def test(self, game, weights):
 	
 		# Get current board state	
 		board = game.getBoard().flatten()
@@ -47,8 +48,8 @@ class NeuralNetwork():
 		
 
 		# Create net inputs and outputs containing the results of each layer
-		net_inputs = np.ones(weights.shape)
-		outputs = np.ones(weights.shape)
+		net_inputs = np.zeros(weights.shape, dtype=np.float)
+		outputs = np.zeros(weights.shape, dtype=np.float)
 	
 		# Weights are NxM while board is 1xM
 		num_layers = weights.shape[0]
@@ -58,34 +59,38 @@ class NeuralNetwork():
 
 			# Get's the weights in each layer
 			temp_weights = weights[i,:]
-			
-			# Total sum of the previous layer
-			sum_previous = 0
-			
-			# First layer has sum_previous be the sum of inputs
+
+			# First layer run needs to use board as input
 			if i == 0:
-				sum_previous = np.add(board)
-
-			# Set sum_previous to sum of previous row's outputs
+				layer_1 = board				
+					
+			# Following layer runs need to use the output of the previous layer as input
 			else:
-				sum_previous = np.add(outputs)
+				layer_1 = outputs[i-1,:]
 
-			# Add non-sigmoid input values
-			net_inputs[i,:] = np.mult(num_previous, temp_weights)
+			# Add each input and output its list
+			for j in range(outputs.shape[1]):
+				for k in range(outputs.shape[1]):
 
-			# Add sigmoid output values
-			outputs[i,:] = sigmoid(np.mult(sum_previous, temp_weights))
+					# Calculate weight index: index = output_index*num_outputs + input_index
+					index = j*outputs.shape[1] + k
+
+					# Add each layer_1*weight[i, index] value to the input list
+					net_inputs[i, j] += weights[i, index]*layer_1[k]
+
+				# Add each sigmoid(layer_1) value to the output list
+				outputs[i, j] = sigmoid(net_inputs[i,j])
 
 		# Return the outputs
 		return outputs, net_inputs
 
-	def train(game, weights):
+	def train(self, game, weights):
 
 		# Get the new game board state
 		board = game.getBoard().flatten()
 
 		# Feed forward for output values
-		outputs, net_inputs = test(game, board.flatten(), weights)
+		outputs, net_inputs = self.test(game, weights)
 
 		# Get the best output index
 		index = 0
@@ -108,22 +113,25 @@ class NeuralNetwork():
 		# Good move:	1 - Board changes and not a bomb
 		# Bad move:	0 - Bomb found
 		for i in range(len(board)):
-			if not board[i] == new_board[i] and not game.gameStatus() == -1
+			if not board[i] == new_board[i] and not game.gameStatus() == -1:
 				expected_values[i] = 1
-			if newBoard[i] == -2
+			if newBoard[i] == -2:
 				expected_values[i] = 0
 
 		# Get new weights after backpropagating
-		new_weights = backpropagate(inputs, weights, outputs, expected_values)
+		new_weights = self.backpropagate(inputs, weights, outputs, expected_values)
 
 		# Calculate weight differences
 		old_error = calc_total_error(expected_values, outputs)
-		new_outputs = test(game, new_weights)
+		new_outputs = self.test(game, new_weights)
 		new_error = calc_total_error(expected_values, new_outputs)
 
 		# New error should always be lower than the old error
 		if new_error > old_error:
 			print 'You fucked up somewhere...\n'
+
+		# Print out the error
+		print new_error + '\n'
 
 		# Update the weights based on the expected values
 		return new_weights
@@ -193,48 +201,48 @@ class NeuralNetwork():
 
 	# Updates the weights based on the expected values and output values
 	# TODO(Alex): Currently only does output layer. Extend to each layer to adjust all values and finish backpropagate.
-	def update_weights(weights, outputs, net_inputs, expected_values):
-		global learning_rate
-
-		num_layers = weights.shape[0]
-
-		# We will store our adjusted weights here
-		adjusted_weights = np.zeros(weights.shape, dtype=weights.dtype)
-
-		# Count down the layers: out->layer2->layer1
-		for i in range(num_layers, 0, -1):
-			# Elements in each layer
-			for j in range(len(outputs)):
-
-				expected_value = output[i, j];
-				output = output[i, j]
-
-				for k in range(weights.shape[1]):
-					weight_delta = change_error_wrt_weight(weights[k], 
-
-		# Start from output perceptrons and work backwards
-		for i in range(len(outputs)):
-			# Set up initial variables
-			expected_value = outputs[i]
-			output = outputs[i]
-
-			# Update each weight
-			for j in range(weights.shape[1]):
-
-				# Amount we adjust weight by, use original weights
-				weight_delta = change_error_wrt_weight(weights[j], expected_value, output)
-
-				# Haven't made any adjustments yet
-				if adjusted_weights[num_layers-1, j] == 0:
-
-					# Add new weight to adjusted weight list based on delta and learning rate
-					adjusted_weights[num_layers-1, j] += weights[num_layers-1, h] - learning_rate*weight_delta
-
-				# Make adjustments to adjusted weight
-				else:
-					# Adjust adjusted weight by delta and learning rate
-					adjusted_weights[num_layers-1, j] -= learning_rate*weight_delta
-
-
-
-		
+#	def update_weights(weights, outputs, net_inputs, expected_values):
+#		global learning_rate
+#
+#		num_layers = weights.shape[0]
+#
+#		# We will store our adjusted weights here
+#		adjusted_weights = np.zeros(weights.shape, dtype=weights.dtype)
+#
+#		# Count down the layers: out->layer2->layer1
+#		for i in range(num_layers, 0, -1):
+#			# Elements in each layer
+#			for j in range(len(outputs)):
+#
+#				expected_value = output[i, j];
+#				output = output[i, j]
+#
+#				for k in range(weights.shape[1]):
+#					weight_delta = change_error_wrt_weight(weights[k], 
+#
+#		# Start from output perceptrons and work backwards
+#		for i in range(len(outputs)):
+#			# Set up initial variables
+#			expected_value = outputs[i]
+#			output = outputs[i]
+#
+#			# Update each weight
+#			for j in range(weights.shape[1]):
+#
+#				# Amount we adjust weight by, use original weights
+#				weight_delta = change_error_wrt_weight(weights[j], expected_value, output)
+#
+#				# Haven't made any adjustments yet
+#				if adjusted_weights[num_layers-1, j] == 0:
+#
+#					# Add new weight to adjusted weight list based on delta and learning rate
+#					adjusted_weights[num_layers-1, j] += weights[num_layers-1, h] - learning_rate*weight_delta
+#
+#				# Make adjustments to adjusted weight
+#				else:
+#					# Adjust adjusted weight by delta and learning rate
+#					adjusted_weights[num_layers-1, j] -= learning_rate*weight_delta
+#
+#
+#
+#		
