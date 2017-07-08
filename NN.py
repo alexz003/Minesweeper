e4d1ec8236bda0d67c9405b80f7b174ad778bcda
Alex <johnalexmarich@gmail.com>
2017-07-08 01:27:21 -0400

    Added backpropagation. TODO: Major cleanup

--- NN.py
+++ NN.py
@@ -2,8 +2,6 @@ import numpy as np
 
 class NeuralNetwork():
 			
-	learning_rate = 0.1
-
 	# Sigmoid with vectors
 	def sigmoid(x):
 		return 1/(1 + np.exp(x))
@@ -16,7 +14,7 @@ class NeuralNetwork():
 	def create_model(num_layers = 1, nodes_per_layer, num_trials=10000):
 
 		# NxM weights matrix with random initial values
-		weights = np.random.random_sample((num_layers, nodes_per_layer))
+		weights = np.random.random_uniform((num_layers, nodes_per_layer))
 		
 		# Output matrix
 		output = np.zeros(num_inputs)
@@ -41,14 +39,19 @@ class NeuralNetwork():
 	
 		# Get current board state	
 		board = game.getBoard().flatten()
+	
+		# TODO(Alex): Currently only outputs total out. To save future computation,
+		#		we can save the net input and output of each layer and output
+		#		those arrays instead
+		
 
-		# Create outputs with 1's so we can multiply by initial values
-		outputs = np.ones(len(board))
-		net_inputs = np.ones(len(board))
-
+		# Create net inputs and outputs containing the results of each layer
+		net_inputs = np.ones(weights.shape)
+		outputs = np.ones(weights.shape)
+	
 		# Weights are NxM while board is 1xM
 		num_layers = weights.shape[0]
-		
+	
 		# Go through each layer
 		for i in range(num_layers):
 
@@ -66,11 +69,11 @@ class NeuralNetwork():
 			else:
 				sum_previous = np.add(outputs)
 
-			# Multiply current weight by sum_previous, then take the sigmoid
-			outputs = sigmoid(np.mult(sum_previous, temp_weights))
+			# Add non-sigmoid input values
+			net_inputs[i,:] = np.mult(num_previous, temp_weights)
 
-			# Non-sigmoid output values
-			net_inputs = np.mult(num_previous, temp_weights)
+			# Add sigmoid output values
+			outputs[i,:] = sigmoid(np.mult(sum_previous, temp_weights))
 
 		# Return the outputs
 		return outputs, net_inputs
@@ -97,24 +100,41 @@ class NeuralNetwork():
 			if newBoard[i] == -2
 				expected_values[i] = 0
 
+		# Get new weights after backpropagating
+		new_weights = backpropagate(inputs, weights, outputs, expected_values)
+
+		old_error = calc_total_error(expected_values, outputs)
+		new_outputs = test(game, new_weights)
+		new_error = calc_total_error(expected_values, new_outputs)
+
+		if new_error > old_error:
+			print 'You fucked up somewhere...\n'
+
 		# Update the weights based on the expected values
-		return update_weights(weights, outputs, net_inputs, expected_value)
+		return new_weights
+
+	# Calculate total error
+	def calc_total_error(expected_values, outputs):
+		return 1/2*np.sum(np.power(expected_values - outputs, 2))
 
 	# Get an array containing the squared error between the expected value, and actual values
-	def total_layer_error(expected_values, outputs):
-		return 1/2*np.sum(np.power(expected_value - output, 2))
+	def total_layer_errors(expected_values, outputs):
+		return 1/2*np.power(expected_values - outputs, 2)
 
 	def change_error_wrt_output(expected_value, output):
 		return -(expected_value - output)
 
 	# Reverts the activation function so we can adjust properly
 	def change_output_wrt_netinput(output):
-		return outputs*(1-output)
+		return output*(1-output)
 
 	# How much net input changes due to weights
 	# While unnecessary to define, I figured writing it would actually show its value
 	def change_netinput_wrt_weight(hidden_output):
 		return hidden_output
+
+	def change_error_wrt_prev_hidden():
+		change_error_wrt_output(expected_value, output)*change_output_wrt_netinput(expected_value, output)
 
 	# How the error changes based on weight aka our delta
 	# dE/dt = dE/dO * dO/dN * dN/dW
@@ -124,6 +144,38 @@ class NeuralNetwork():
 				change_output_wrt_netinput(output)*
 					change_netinput_wrt_weight(weight))
 	
+
+	def backpropagate(inputs, weights, outputs, expected_values, learning_rate=0.1):	
+
+		new_weights = np.zeros(weights.shape, dtype=np.int32)
+
+		dels = np.zeros(outputs.shape, dtype=outputs.dtype)
+		dels[dels.shape[0]-1,:] = total_layer_errors(expected_values, outputs[outputs.shape[0]-1,:])
+
+
+		# The following nested loops should set up out del values for what we need to change 
+		# Loop each layer
+		for i in range(dels.shape[0]-1, 0, -1):
+
+			# Get each node in this layer
+			for j in range(dels.shape[1]-1):
+
+				# Adds previous dels and weights to this del value (fully connected)
+				for k in range(dels.shape[1]-1):
+					dels[i-1, j] += dels[i,k]*weights[i,k]
+
+				# Multiply out dels by the do/di	
+				dels[i-1, j] *= change_output_wrt_netinput(outputs[i-1, j])			
+
+				# Multiply by inputs or outputs of previous layer(the inputs to this layer)
+				if i == 1:
+					dels[i-1, j] *= inputs[j]
+				else:
+					dels[i-1, j] *= outputs[i-1, j]
+
+		# Return updated weights including learning rate
+		return weights + learning_rate*dels
+
 	# Updates the weights based on the expected values and output values
 	# TODO(Alex): Currently only does output layer. Extend to each layer to adjust all values and finish backpropagate.
 	def update_weights(weights, outputs, net_inputs, expected_values):
@@ -133,6 +185,17 @@ class NeuralNetwork():
 
 		# We will store our adjusted weights here
 		adjusted_weights = np.zeros(weights.shape, dtype=weights.dtype)
+
+		# Count down the layers: out->layer2->layer1
+		for i in range(num_layers, 0, -1):
+			# Elements in each layer
+			for j in range(len(outputs)):
+
+				expected_value = output[i, j];
+				output = output[i, j]
+
+				for k in range(weights.shape[1]):
+					weight_delta = change_error_wrt_weight(weights[k], 
 
 		# Start from output perceptrons and work backwards
 		for i in range(len(outputs)):
